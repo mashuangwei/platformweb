525e7f85bc041e109b47399a26f12c13c88570c7
mashuangwei <mashuangwei@aliyun.com>
2017-07-21 10:45:16 +0800

    add

--- src/components/content/test.vue
+++ src/components/content/test.vue
@@ -1,9 +1,45 @@
 <template>
-  <div>
-    abc
+  <div class="echarts">
+      <div id="mychart" :style="{width:'300px',height:'300px'}"/>
+      test......
   </div>
 </template>
 
 <script>
-  export default {}
+//  import echarts from 'echarts'
+  // 引入 ECharts 主模块
+  var echarts = require('echarts/lib/echarts')
+  // 引入柱状图
+  require('echarts/lib/chart/bar')
+  // 引入提示框和标题组件
+  require('echarts/lib/component/tooltip')
+  require('echarts/lib/component/title')
+  export default {
+    components: {
+      echarts
+    },
+    name: 'chart',
+    options: {
+      title: {
+        text: 'ECharts 入门示例'
+      },
+      tooltip: {},
+      legend: {
+        data: ['销量']
+      },
+      xAxis: {
+        data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', ' 袜子']
+      },
+      yAxis: {},
+      series: [{
+        name: '销量',
+        type: 'bar',
+        data: [5, 20, 36, 10, 10, 20]
+      }]
+    },
+    mounted () {
+      let myChart = this.$echarts.init(document.getElementById('myChart'))
+      myChart.setOption(this.options)
+    }
+  }
 </script>
