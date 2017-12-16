7fd174364fd265c3ada36dd28d449985c5f99a19
mashuangwei <mashuangwei@aliyun.com>
2017-12-16 23:20:03 +0800

    add opboss

---
+++ src/components/content/opboss/audio.vue
@@ -0,0 +1,22 @@
+<template>
+  <audio :src="src" controls="controls" style="width:100%;">
+    {{ name }}
+  </audio>
+</template>
+
+<script>
+export default {
+  props: {
+    src: {
+      default () {
+        return ''
+      }
+    },
+    name: {
+      default () {
+        return 'wav文件'
+      }
+    }
+  }
+}
+</script>
